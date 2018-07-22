5ddd64d3e2f0a5fd2096debc33eeb31afc06bc20
Shawn Toffel <shawn.toffel@gmail.com>
2018-07-22 23:58:37 +0000

    Added http transport

--- cmd/services/registration/main.go
+++ cmd/services/registration/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	pb "github.com/jukeizu/treediagram/api/registration"
 	"github.com/jukeizu/treediagram/services/registration"
@@ -21,7 +24,8 @@ func init() {
 }
 
 type Config struct {
-	Port           int
+	HttpPort       int
+	GrpcPort       int
 	CommandStorage mdb.DbConfig
 }
 
@@ -39,24 +43,49 @@ func main() {
 		panic(err)
 	}
 
-	logger.Log("storage connected", "true")
-
 	service, err := registration.NewService(storage)
 	if err != nil {
 		panic(err)
 	}
 
-	s := grpc.NewServer()
+	errChannel := make(chan error)
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT)
+		errChannel <- fmt.Errorf("%s", <-c)
+	}()
 
-	pb.RegisterRegistrationServer(s, registration.GrpcBinding{Service: service})
+	go func() {
+		port := fmt.Sprintf(":%d", config.GrpcPort)
 
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", config.Port))
-	if err != nil {
-		logger.Log("error", err.Error())
-	}
+		listener, err := net.Listen("tcp", port)
+		if err != nil {
+			logger.Log("error", err.Error())
+		}
 
-	err = s.Serve(listener)
-	if err != nil {
-		logger.Log("error", err.Error())
-	}
+		s := grpc.NewServer()
+
+		pb.RegisterRegistrationServer(s, registration.GrpcBinding{Service: service})
+
+		logger.Log("transport", "grpc", "address", port, "msg", "listening")
+		errChannel <- s.Serve(listener)
+	}()
+
+	go func() {
+		port := fmt.Sprintf(":%d", config.HttpPort)
+
+		handler := registration.MakeHandler(service, logger)
+
+		mux := http.NewServeMux()
+		mux.Handle("/add", handler)
+		mux.Handle("/disable", handler)
+		mux.Handle("/query", handler)
+
+		http.Handle("/", mux)
+
+		logger.Log("transport", "http", "address", port, "msg", "listening")
+		errChannel <- http.ListenAndServe(port, nil)
+	}()
+
+	logger.Log("stopped", <-errChannel)
 }
--- services/registration/transport.go
+++ services/registration/transport.go
@@ -1,25 +1,130 @@
 package registration
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-kit/kit/log"
+	httpTransport "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+)
 
 type Command struct {
-	Id             bson.ObjectId `bson:"_id, omitempty"`
-	Server         string
-	Name           string
-	Regex          string
-	RequireMention bool
-	Endpoint       string
-	Help           string
-	Enabled        bool
+	Id             bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+	Server         string        `json:"server"`
+	Name           string        `json:"name"`
+	Regex          string        `json:"regex"`
+	RequireMention bool          `json:"requireMention"`
+	Endpoint       string        `json:"endpoint"`
+	Help           string        `json:"help"`
+	Enabled        bool          `json:"enabled"`
 }
 
 type CommandQuery struct {
-	Server   string
-	LastId   string
-	PageSize int
+	Server   string `json:"server"`
+	LastId   string `json:"lastId"`
+	PageSize int    `json:"pageSize"`
 }
 
 type CommandQueryResult struct {
-	Commands []Command
-	HasMore  bool
+	Commands []Command `json:"commands"`
+	HasMore  bool      `json:"hasMore"`
+}
+
+type AddRequest struct {
+	Command Command `json:"command"`
+}
+
+type DisableRequest struct {
+	Id string `json:"id"`
+}
+
+func DecodeAddRequest(_ context.Context, r *http.Request) (request interface{}, err error) {
+	addRequest := AddRequest{}
+
+	err = json.NewDecoder(r.Body).Decode(&addRequest)
+
+	if err != nil {
+		return nil, err
+
+	}
+
+	return addRequest, nil
+}
+
+func DecodeDisableRequest(_ context.Context, r *http.Request) (request interface{}, err error) {
+	disableRequest := DisableRequest{}
+
+	err = json.NewDecoder(r.Body).Decode(&disableRequest)
+
+	if err != nil {
+		return nil, err
+
+	}
+
+	return disableRequest, nil
+}
+
+func DecodeQueryRequest(_ context.Context, r *http.Request) (request interface{}, err error) {
+	query := CommandQuery{}
+
+	err = json.NewDecoder(r.Body).Decode(&query)
+
+	if err != nil {
+		return nil, err
+
+	}
+
+	return query, nil
+}
+
+func EncodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
+func EncodeError(_ context.Context, err error, w http.ResponseWriter) {
+	w.WriteHeader(http.StatusInternalServerError)
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"error": err.Error(),
+	})
+}
+
+func MakeHandler(s Service, logger log.Logger) http.Handler {
+	opts := []httpTransport.ServerOption{
+		httpTransport.ServerErrorLogger(logger),
+		httpTransport.ServerErrorEncoder(EncodeError),
+	}
+
+	addMessageHandler := httpTransport.NewServer(
+		MakeAddRequestEndpoint(s),
+		DecodeAddRequest,
+		EncodeResponse,
+		opts...,
+	)
+
+	disableMessageHandler := httpTransport.NewServer(
+		MakeDisableRequestEndpoint(s),
+		DecodeDisableRequest,
+		EncodeResponse,
+		opts...,
+	)
+
+	queryMessageHandler := httpTransport.NewServer(
+		MakeQueryRequestEndpoint(s),
+		DecodeQueryRequest,
+		EncodeResponse,
+		opts...,
+	)
+	router := mux.NewRouter()
+
+	router.Handle("/add", addMessageHandler).Methods("POST")
+	router.Handle("/disable", disableMessageHandler).Methods("POST")
+	router.Handle("/query", queryMessageHandler).Methods("POST")
+
+	return router
 }
