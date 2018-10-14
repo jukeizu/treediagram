64f0b88c52be0613fefb203823ea1af37ce27f80
Shawn Toffel <shawn.toffel@gmail.com>
2018-10-14 20:49:34 +0000

    cleanup

--- services/registration/http.go
+++ services/registration/http.go
@@ -20,6 +20,35 @@ func NewHttpBinding(logger log.Logger, s pb.RegistrationServer) httpBinding {
 	return httpBinding{logger, s}
 }
 
+func (h httpBinding) MakeHandler() http.Handler {
+	addMessageHandler := transport.NewDefaultServer(
+		h.logger,
+		h.addCommandEndpoint,
+		DecodeAddRequest,
+	)
+
+	disableMessageHandler := transport.NewDefaultServer(
+		h.logger,
+		h.disableCommandEndpoint,
+		DecodeDisableRequest,
+	)
+
+	queryMessageHandler := transport.NewDefaultServer(
+		h.logger,
+		h.queryCommandEndpoint,
+		DecodeQueryRequest,
+	)
+
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/registration/").Subrouter()
+
+	subrouter.Handle("/add", addMessageHandler).Methods("POST")
+	subrouter.Handle("/disable", disableMessageHandler).Methods("POST")
+	subrouter.Handle("/query", queryMessageHandler).Methods("POST")
+
+	return subrouter
+}
+
 func (h httpBinding) addCommandEndpoint(ctx context.Context, r interface{}) (interface{}, error) {
 	request := r.(pb.AddCommandRequest)
 
@@ -60,33 +89,4 @@ func DecodeQueryRequest(_ context.Context, r *http.Request) (interface{}, error)
 	err := json.NewDecoder(r.Body).Decode(&request)
 
 	return request, err
-}
-
-func (h httpBinding) MakeHandler() http.Handler {
-	addMessageHandler := transport.NewDefaultServer(
-		h.logger,
-		h.addCommandEndpoint,
-		DecodeAddRequest,
-	)
-
-	disableMessageHandler := transport.NewDefaultServer(
-		h.logger,
-		h.disableCommandEndpoint,
-		DecodeDisableRequest,
-	)
-
-	queryMessageHandler := transport.NewDefaultServer(
-		h.logger,
-		h.queryCommandEndpoint,
-		DecodeQueryRequest,
-	)
-
-	router := mux.NewRouter()
-	subrouter := router.PathPrefix("/registration/").Subrouter()
-
-	subrouter.Handle("/add", addMessageHandler).Methods("POST")
-	subrouter.Handle("/disable", disableMessageHandler).Methods("POST")
-	subrouter.Handle("/query", queryMessageHandler).Methods("POST")
-
-	return subrouter
 }
