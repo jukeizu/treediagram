7279ee12b42899f0b5bb3f7a5d6920c074b2442c
Shawn Toffel <shawn.toffel@gmail.com>
2019-02-10 06:46:43 +0000

    Added endpoint error handling to processor client

--- processor/client.go
+++ processor/client.go
@@ -3,9 +3,11 @@ package processor
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/jukeizu/contract"
 )
 
 type Client struct{}
@@ -13,15 +15,19 @@ type Client struct{}
 func (c Client) Do(request interface{}, endpoint string) (string, error) {
 	body, err := json.Marshal(request)
 	if err != nil {
-		return "", errors.New("could not marshal request to json: " + err.Error())
+		return "", fmt.Errorf("could not marshal request to json: %s", err.Error())
 	}
 
 	resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(body))
 	if err != nil {
-		return "", errors.New("error sending request: " + err.Error())
+		return "", fmt.Errorf("error sending request: %s", err.Error())
 	}
 
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", buildError(resp)
+	}
 
 	b, err := ioutil.ReadAll(resp.Body)
 
@@ -30,4 +36,18 @@ func (c Client) Do(request interface{}, endpoint string) (string, error) {
 	}
 
 	return string(b), nil
+}
+
+func buildError(resp *http.Response) error {
+	if resp.StatusCode != http.StatusInternalServerError {
+		return fmt.Errorf("status code: %d", resp.StatusCode)
+	}
+
+	errorMessage := contract.Error{}
+	err := json.NewDecoder(resp.Body).Decode(&errorMessage)
+	if err != nil {
+		return fmt.Errorf("could not decode error: %s", err.Error())
+	}
+
+	return fmt.Errorf(errorMessage.Message)
 }
